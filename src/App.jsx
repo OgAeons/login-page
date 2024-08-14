fc8b7254cde6ec576e85ab3a73c799b21e36fb8f
OgAeons <opaeons@gmail.com>
2024-08-14 17:42:32 +0530

    Added html structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,42 @@ import React from "react"
 
 function App() {
     return (
-        <div>
-            
+        <div className="container">
+            <img src="" alt="" />
+            <div className="signup-container">
+                <h2>Sign In to Artificial Intelligence Medicare</h2>
+                <h5>New here? <span>Create account</span></h5>
+                <label for="email">Email</label>
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="email" placeholder="Email Address" name="email"/>
+                </div>
+                <label for="email">Password</label>
+                <div className="input">
+                    <img src="" alt="" />
+                    <input type="text" placeholder="Password" name="password"/>
+                </div>
+                <h5>Forgot pasword?</h5>
+                <button type="submit">Sign In</button>
+                <div className="or">
+                    <img src="" alt="" />
+                    <p> OR </p>
+                    <img src="" alt="" />
+                </div>
+                <div className="Auth">
+                    <img src="" alt="" />
+                    <button type="submit">Sign in with Google</button>
+                </div>
+                <div className="Auth">
+                    <img src="" alt="" />
+                    <button type="submit">Sign in with Facebook</button>
+                </div>
+                <div className="Auth">
+                    <img src="" alt="" />
+                    <button type="submit">Sign in with X</button>
+                </div>
+                
+            </div>
         </div>
     )
 }
