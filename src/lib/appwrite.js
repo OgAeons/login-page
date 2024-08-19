d907d55e2b3d13c5258d2ef633b626305ffb1b61
OgAeons <opaeons@gmail.com>
2024-08-19 18:39:01 +0530

    Made chnages to the appwrtie.js

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account} from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('http://localhost/v1')
-    .setProject('66c1ea050006d7f81a50');
+    .setEndpoint('your endpoint url')
+    .setProject('your project id');
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
